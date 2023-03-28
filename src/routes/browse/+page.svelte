a9ead90b7abfd9702529eba80d1150d90f1bfaba
Merkie <merkie@post.com>
2023-03-28 18:37:24 -0500

    add more functionality to browse page + added cols to tierlist model

--- src/routes/browse/+page.svelte
+++ src/routes/browse/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { disableScrollHandling } from '$app/navigation';
 	import type { Tierlist, SortableItem } from '@prisma/client';
 	import { TierlistCategories, TierlistName, TierlistItems } from '../../helpers/stores';
 	export let data: { tierlists: Tierlist[] };
@@ -21,9 +22,30 @@
 				imageurl: item.imageurl
 			};
 		});
+
+		window.location.assign('/');
 	};
 </script>
 
-{#each data.tierlists as tierlist}
-	<button on:click={async () => await loadTierlist(tierlist.id)}>{tierlist.name}</button>
-{/each}
+<div class="flex-1 grid grid-cols-4 gap-1 p-1 pt-0">
+	{#each data.tierlists as tierlist}
+		<div class="bg-[#1a1a17] h-fit p-2 flex flex-col gap-2 border border-zinc-800 rounded-md">
+			<div class="bg-black relative h-[200px]">
+				<img
+					class="absolute h-[100%] object-fit left-1/2 -translate-x-1/2"
+					src={tierlist.imageurl}
+					alt={tierlist.name}
+				/>
+			</div>
+			<h1 class="text-lg font-medium text-zinc-200">{tierlist.name}</h1>
+			<p class="text-zinc-400">{tierlist.description}</p>
+			<div class="flex">
+				<div class="flex-1" />
+				<button
+					on:click={async () => await loadTierlist(tierlist.id)}
+					class="bg-blue-500 px-4 p-2 rounded-md">Launch</button
+				>
+			</div>
+		</div>
+	{/each}
+</div>
